d3edb2339790a3d833b7bfb7f139ca86855fec1f
Nahom Ambachew <101961314+Nahilor@users.noreply.github.com>
2026-06-20 15:01:05 +0300

    Update JsonParser.java

--- src/main/java/com/github/nahilor/JsonParser.java
+++ src/main/java/com/github/nahilor/JsonParser.java
@@ -1,6 +1,7 @@
 package com.github.nahilor;
 
-// consider using a hashmap
+// possibly gonna use hashmaps to represent json
+// for this project hashmap is sufficient but for a json file with multiple levels of nesting using a hashmap would be a bad idea.
 public class JsonParser {
 
 }
